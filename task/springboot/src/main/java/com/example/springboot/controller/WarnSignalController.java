c1aef12cee576c88c125805a556fa386f461adec
thomos king <3480736232@qq.com>
2025-05-19 13:20:07 +0800

    feat:调整业务代码和技术方案

--- task/springboot/src/main/java/com/example/springboot/controller/WarnSignalController.java
+++ task/springboot/src/main/java/com/example/springboot/controller/WarnSignalController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@Api(tags = "预警信号管理")
+@Api(tags = "电池信号管理")
 @CrossOrigin
 @RequestMapping("/signal")
 public class WarnSignalController {
@@ -22,43 +22,43 @@ public class WarnSignalController {
     private WarnSignalService warnSignalService;
 
     @PostMapping("/add")
-    @ApiOperation(value = "添加预警信号信息")
+    @ApiOperation(value = "添加电池信号信息")
     public Result add(@RequestBody WarnSignalDto warnSignalDto){
         //现在尝试老师的添加方法
         return warnSignalService.add(warnSignalDto) ;
     }
     @PostMapping("/addList")
-    @ApiOperation(value = "添加一个列表的预警信号信息")
+    @ApiOperation(value = "添加一个列表的电池信号信息")
     public Result addList(@RequestBody List<WarnSignalDto> warnSignalDtos){
         //现在尝试老师的添加方法
         return warnSignalService.addList(warnSignalDtos) ;
     }
     @PostMapping("/get")
-    @ApiOperation("获取预警信号信息")
+    @ApiOperation("获取电池信号信息")
     public Result get(@RequestParam String id)
     {
         return warnSignalService.get(id);
     }
     @PostMapping("/getByCarId")
-    @ApiOperation("根据车架id获取预警信号信息")
+    @ApiOperation("根据车架id获取电池信号信息")
     public Result getByCarId(@RequestParam String carId)
     {
         return warnSignalService.getByCarId(carId);
     }
     @GetMapping("/getAll")
-    @ApiOperation("获取所有预警信号信息")
+    @ApiOperation("获取所有电池信号信息")
     public Result getAll()
     {
         return warnSignalService.getAll();
     }
     @PostMapping("/delete")
-    @ApiOperation("删除预警信号信息")
+    @ApiOperation("删除电池信号信息")
     public Result delete(@RequestParam String id)
     {
         return warnSignalService.delete(id);
     }
     @PostMapping("/update")
-    @ApiOperation(value = "更改预警信号信息")
+    @ApiOperation(value = "更改电池信号信息")
     @Transactional // 添加事务注解
     public Result updateWarnSignal(@RequestBody WarnSignal warnSignal){
         return warnSignalService.updateWarnSignal(warnSignal) ;
